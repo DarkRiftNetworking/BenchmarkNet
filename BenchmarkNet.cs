54ddeaab438b992291700b9984458be0242c52bf
nxrighthere <nxrighthere@users.noreply.github.com>
2018-01-22 19:36:50 +0500

    Revert change of GC latency mode

--- BenchmarkNet.cs
+++ BenchmarkNet.cs
@@ -24,7 +24,6 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -191,8 +190,6 @@ namespace BenchmarkNet {
 			Console.Clear();
 			
 			processActive = true;
-			
-			GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
 
 			if (selectedLibrary == 0)
 				Library.Initialize();
